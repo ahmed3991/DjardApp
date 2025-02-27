6813689651eb35290b730fb0fc56e576f614d907
Ahmed <ahmed.g@turing.com>
2025-02-27 10:26:42 +0100

    first commit

---
+++ app/src/main/java/com/fdc/djardapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.fdc.djardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.fdc.djardapp.ui.theme.DjardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DjardAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "FDC Team !!!",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        fontSize = 36.sp,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DjardAppTheme {
+        Greeting("Android")
+    }
+}
